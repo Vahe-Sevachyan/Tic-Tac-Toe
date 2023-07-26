e328b83603ff3877b253b72ba4ba85ddc5f81d4a
Vahe Sevachyan <vscode13@gmail.com>
2023-07-25 19:45:03 -0700

    prepared gameboard and styles

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-const displayBoxes = document.querySelectorAll(".box");
+const gameBoard = document.querySelector(".game-board");
 
-const boxes = document.querySelectorAll(".box");
+const info = document.querySelector(".info");
 
-const gameBoard = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
-];
-
-boxes.forEach((box) => {
-  box.onclick = (e) => {
-    if (e.target.innerHTML === "") {
-      e.target.innerHTML = "v";
-    }
-  };
-});
-
-for (let i = 0; i < gameBoard.length; i++) {
-  for (let j = 0; j < gameBoard[i].length; j++) {
-    const gameBoardValue = gameBoard[i][j];
-    displayBoxes.forEach((box) => {
-      box.innerHTML = gameBoardValue;
-    });
-  }
+const boardValues = ["", "", "", "", "", "", "", "", ""];
+function createBoard() {
+  boardValues.forEach((_boardValue, index) => {
+    const boardElement = document.createElement("div");
+    boardElement.classList.add("square");
+    gameBoard.append(boardElement);
+  });
 }
-gameBoard.forEach((game) => {
-  console.table(game);
-});
-// function boardDisplay(displayBoxes) {
+createBoard();
 
+// const displayBoxes = document.querySelectorAll(".box");
+
+// const boxes = document.querySelectorAll(".box");
+
+// const gameBoard = [
+//   ["", "", ""],
+//   ["", "", ""],
+//   ["", "", ""],
+// ];
+
+// boxes.forEach((box) => {
+//   box.onclick = (e) => {
+//     if (e.target.innerHTML === "") {
+//       e.target.innerHTML = "v";
+//     }
+//   };
+// });
+
+// for (let i = 0; i < gameBoard.length; i++) {
+//   for (let j = 0; j < gameBoard[i].length; j++) {
+//     const gameBoardValue = gameBoard[i][j];
+//     displayBoxes.forEach((box) => {
+//       box.innerHTML = gameBoardValue;
+//     });
+//   }
 // }
+// gameBoard.forEach((game) => {
+//   console.table(game);
+// });
 
 // displayBoxes.forEach((box) => {
 //   console.log(box.innerHTML);
